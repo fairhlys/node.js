9e436e975467bba1e3cfa484e14385dae9a92796
faith <3096778372@qq.com>
2026-02-08 22:11:44 +0800

    2-8

--- 09Express框架.js
+++ 09Express框架.js
@@ -10,8 +10,7 @@ express框架：
             res.end()
           })
         如果请求方法是get，请求url是/home，那么就会触发该回调函数
-        app.all()这里是请求方法随意，只要url正确就行
-        app.all('*')这里是请求方法和url随意，一般用于404 NOT FOUND
+        aapp.use() 专门用来处理中间件和“兜底”路由
     4.监听端口，启动服务
         app.listen(端口号,() => {
             服务启动，执行该回调函数
@@ -40,15 +39,28 @@ express框架：
         res.status()；设置响应状态码
         res.set('abc', 'qwe');设置响应头参数
         res.send();设置响应体数据，自动会设置charset=utf-8
-        res.redirect(url网址):重定向
+        res.redirect(url网址):重定向,会发送请求
         res.download(文件地址):下载响应
         res.json(对象)
         res.sendFile(文件路径);把文件内容发送到响应体中
+        同一个请求的响应只能发送一次（只能调用一次 res.send、res.json、res.end、res.redirect 等会真正发送响应的方法）。
     9.中间件
         middleware中间件本质是一个回调函数
         中间件函数可以像路由回调一样访问请求对象，响应对象。就是路由拦截器
         1.全局中间件
+            1.定义中间件函数
+            function recordMiddleware(req,res,next) {
+                req是截获的请求报文
+                res是截获的响应报文
+                next是一个函数，指向后续的路由回调，或者是中间件函数回调(也就是执行了中间件函数之后，还要执行对应app.get等的函数)
+                next()
+            }
+            2.然后使用中间件函数
+            app.use(recordMiddleware)
         2.路由中间件
+        3.静态资源中间件：
+         app.use(express.static(__dirname + 静态资源目录))
+         设置了访问静态资源的目录,当要寻找静态资源时，自动会在该目录下寻找
     
 express路由：
     一个路由的组成有请求方法，路径和回调函数组成
@@ -60,6 +72,7 @@ const app = express()
 app.get('/home', (req, res) => {
     res.end('hello express')
 })
+
 app.listen(3000, () => {
     console.log('服务启动，端口号3000')
 })
--- 包管理/index.js
+++ 包管理/index.js
@@ -1,16 +1,50 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+// 日志中间件
+function RecordMiddleWare(req, res, next) {
+    const { url, ip } = req;
+    fs.appendFileSync(path.resolve(__dirname, './access.log'), `${url}, ${ip}\r\n`);
+    console.log(req.query);
+    next();
+}
+
+// 暗号校验中间件（全局，但可以排除某些路径）
+function checkCode(req, res, next) {
+    // 如果是根路径，直接放行（示例：根路径不需要暗号）
+    if (req.path === '/') {
+        return next();
+    }
+
+    if (req.query.code === '521') {
+        next();
+    } else {
+        res.send('暗号错误');
+    }
+}
+
+app.use(RecordMiddleWare);
+app.use(checkCode);
+app.use(express.static(path.resolve(__dirname , 'package')))
+app.get('/', (req, res) => {
+    res.send('hello express');
+});
+
 app.get('/home', (req, res) => {
-    res.end('hello express')
-})
-app.get('/',(req,res) => {
-    console.log(req.method);
-    console.log(req.url);
-    console.log(req.path);
-    console.log(req.headers);
-    
-    res.end('home')
-})
+    res.send('home');
+});
+
+app.get('/admin', (req, res) => {
+    res.send('暗号正确');
+});
+
+// 404 兜底
+app.use((req, res) => {
+    res.status(404).send('<h1>404</h1>');
+});
+
 app.listen(3000, () => {
-    console.log('服务启动，端口号3000')
-})
+    console.log('服务启动, 端口号 3000');
+});
